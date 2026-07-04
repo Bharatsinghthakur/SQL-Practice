d36a1aa1020cb23d19bcf23eb800f5788c0a8b26
Bharat Singh Thakur <bharatsinghthakur04@gmail.com>
2026-07-04 20:49:51 +0530

    NESTEDSORTING

--- SQLQuery4.sql
+++ SQLQuery4.sql
@@ -17,3 +17,10 @@ FROM customers
 ORDER BY 
 	country ASC,
 	score DESC
+
+-- Retrieve all customers and sort the results by the country and then by the highest score
+SELECT * 
+FROM customers
+ORDER BY 
+	 country ASC ,
+	 score DESC

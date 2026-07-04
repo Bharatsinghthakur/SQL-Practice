c7f0197dac90eb3fb6c64e8a840cc350d6935ecb
Bharat Singh Thakur <bharatsinghthakur04@gmail.com>
2026-07-04 20:37:08 +0530

    SELECT&FROM&ORDERBY

---
+++ SQLQuery4.sql
@@ -0,0 +1,11 @@
+/* 
+Order by - SORT YOUR DATA 
+ASC -- default
+DESC 
+*/
+
+-- Retrieve all the customers and sort the results by highest score first
+
+SELECT * 
+FROM customers
+ORDER BY score DESC

f935cf33eaefee30bc0d11c02f15fbc1a79b824d
Bharat Singh Thakur <bharatsinghthakur04@gmail.com>
2026-07-07 21:28:34 +0530

    DDL - CREATE TABLE

---
+++ SQLQuery6.sql
@@ -0,0 +1,12 @@
+/*
+Create a new table called persons 
+with columns: id,person_name,birth_date and phone
+*/
+use MyDatabase;
+CREATE TABLE persons(
+	id INT NOT NULL ,
+	person_name VARCHAR(50) NOT NULL ,
+	birth_date DATE,
+	phone VARCHAR(15) NOT NULL
+	CONSTRAINT pk_persons PRIMARY KEY(id) -- pk_persons is internal for DB , id column is primary key
+)

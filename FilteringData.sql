e5b6910d07278ac4db5e9e79857a01649969989b
Bharat Singh Thakur <bharatsinghthakur04@gmail.com>
2026-07-09 22:56:00 +0530

    WHERE-operator-comparison-logical-range-membership-search

--- FilteringData.sql
+++ FilteringData.sql
@@ -70,4 +70,63 @@ WHERE country ='USA' OR score > 500
 SELECT * 
 FROM customers 
 WHERE NOT score < 500
+
+
+/*
+Retrieve all the customers whose score falls in range between 100 and 500
+*/
+ 
+SELECT * 
+FROM customers
+WHERE score BETWEEN 100 AND 500
+
+
+-- shows the explicit boundries 
+SELECT * 
+FROM customers
+WHERE score >=100 AND score <= 500
+
+/*
+Membership operator 
+- IN  - checks if value exists in a list
+- NOT IN - reverse of IN operator
+
+*/
+
+-- Reterieve all cusotmers from USA or germany 
+
+SELECT * 
+FROM customers 
+WHERE country = 'Germany' OR country = 'USA'
+
+SELECT * 
+FROM customers 
+WHERE country IN ('Germany','USA')
+
+/*
+SEARCH operator - search for a pattern in a text 
+
+*/
+
+-- find all the customers whose first name starts with 'M'
+SELECT * 
+FROM customers
+WHERE first_name LIKE 'M%'
+
+-- find all the customers whose first name ends with 'n'
+SELECT * 
+FROM customers 
+WHERE first_name LIKE '%n'
+
+
+-- find all the customers whose first name contains 'r'
+SELECT * 
+FROM customers 
+WHERE first_name LIKE '%r%'
+
 -- 
+-- find all the customers whose first name contains 'r' at 3rd postion
+SELECT * 
+FROM customers 
+WHERE first_name LIKE '__r%'
+

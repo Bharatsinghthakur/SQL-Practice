16a1d94232e96ab2a77aff9c435b0a8552b2a137
Bharat Singh Thakur <bharatsinghthakur04@gmail.com>
2026-07-10 23:30:45 +0530

    Join - innerjoin

--- SQLJOINS.sql
+++ SQLJOINS.sql
@@ -13,6 +13,43 @@ Join types - key column required
 1 - inner join 
 2 - full join 
 3 - left join 
-4 - right join 
+4 - right join
+5 - no join
+
+advanced 
+a - left anti join 
+b - right anti join 
+c - full anti join 
+d - cross join 
  
 */
+
+-- NO JOIN 
+-- Retrieve all data from customers and orders in different results 
+use MyDatabase;
+SELECT * 
+FROM customers;
+
+SELECT * 
+FROM orders;
+
+/*
+
+INNER JOIN - returns only matching rows from both tables 
+-- Default join 
+-- the order of tables doesnt matter in inner join 
+*/
+
+-- get all customers along with their orders, but only for customers who have placed an order
+
+-- TO avoid ambiguity - add table name before the column to 
+-- avoid confusion in join with same named columns 
+
+SELECT 
+     customers.id,
+     customers.first_name,
+     orders.order_id,
+     orders.sales 
+FROM customers 
+INNER JOIN orders 
+ON customers.id = orders.customer_id 

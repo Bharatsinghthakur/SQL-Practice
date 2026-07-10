2e8c61796f0242e3e8687521110292d988fcf797
Bharat Singh Thakur <bharatsinghthakur04@gmail.com>
2026-07-10 21:34:05 +0530

    SQLJoins

---
+++ SQLJOINS.sql
@@ -0,0 +1,18 @@
+/*
+we have two methods through which we can combine table 
+1 - SET operators - it works on rows - should have same number of columns
+2 - Joins  - it works on columns - Key column required 
+
+set types - same number of column required 
+1 - union 
+2- union all
+3 - except (minus)
+4 - intersect
+
+Join types - key column required 
+1 - inner join 
+2 - full join 
+3 - left join 
+4 - right join 
+ 
+*/

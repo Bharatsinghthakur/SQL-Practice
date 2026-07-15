6135ba3cbbc2c550d53a04a3618850f03561d462
Bharat Singh Thakur <bharatsinghthakur04@gmail.com>
2026-07-15 20:47:39 +0530

    SQL-FUNCTIONS-ROW LEVEL FUNCTION

---
+++ RowLevelFunction.sql
@@ -0,0 +1,33 @@
+/*
+SQL FUNCTIONS __ 
+
+
+ROW LEVEL FUNCTION - 
+Sometimes we have to do data cleansing , data transformation and data manipulation 
+in our data . In order to solve some SQL tasks 
+
+- String functions 
+- Number functions 
+- Date & time function 
+- Null function
+- case Statement 
+
+FUNCTION : 
+- A build in SQL code accepts input value 
+- proccess it 
+- returns an output value 
+
+
+SINGLE ROW FUNCTIONS : INPUT for function would be single value and out will be one single 
+value as well 
+Eg: 'BHARAT' - LOWER() - 'bharat'
+
+STRING FUNCTIONS , NUMERIC FUNCTION , DATE & TIME FUNCTION, NULL FUNCTION  
+
+MULTI ROW FUNCTIONS : INPUT is multiple rows and output is single value
+
+AGGREGATE FUNCTION , WINDOW FUNCTION 
+NESTED FUNCTION : FUNCTION INSIDE ANOTHER FUNCTION . WE CAN USE MULTIPLE FUNCTION 
+TOGETHER TO MANIPULATE ONE VALUE.
+
+*/

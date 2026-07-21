8212191880556559495e49bbd255c545893a30de
Bharat Singh Thakur <bharatsinghthakur04@gmail.com>
2026-07-21 21:10:20 +0530

    CASE_STATEMENT

---
+++ Case_Statement.sql
@@ -0,0 +1,21 @@
+/*
+CASE STATEMENT
+Evaluates a list conditions and returns a value when the first condition is met
+
+syntax 
+
+CASE 
+	WHEN condition1 THEN result1  --condition to be evaluated result if condition is true
+	WHEN condition2 THEN result2
+	...
+	ELSE result  -- when none of WHEN conditons are TRUE(OPTIONAL)
+END
+
+----
+CASE
+	WHEN SALES > 50 THEN 'HIGH'
+END
+
+*/
+
+

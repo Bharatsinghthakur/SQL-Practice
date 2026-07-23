ff17216c11f896db1c910e6e56ea9f150350e786
Bharat Singh Thakur <bharatsinghthakur04@gmail.com>
2026-07-23 20:49:28 +0530

    AggregateFunction&analytics

---
+++ Aggregation&AnalyticalFunctions.sql
@@ -0,0 +1,18 @@
+/*
+Aggregate Functions & Analytics 
+
+-- Aggregate functions
+-- Window basics 
+-- window Aggregate functions
+-- window value functions
+*/
+
+/*
+Aggregate functions : 
+Aggregate function accept multiple input and the output will be single value 
+-- COUNT()
+-- SUM()
+-- AVG()
+-- MAX()
+-- MIN()
+*/

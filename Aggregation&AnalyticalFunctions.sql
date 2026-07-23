170ecc3b737c4cdcf15d09ed07cf7d5278cfe70a
Bharat Singh Thakur <bharatsinghthakur04@gmail.com>
2026-07-23 20:57:27 +0530

    Aggregate_function

--- Aggregation&AnalyticalFunctions.sql
+++ Aggregation&AnalyticalFunctions.sql
@@ -10,9 +10,42 @@ Aggregate Functions & Analytics
 /*
 Aggregate functions : 
 Aggregate function accept multiple input and the output will be single value 
--- COUNT()
--- SUM()
--- AVG()
--- MAX()
--- MIN()
+-- COUNT():
+    It will give the total no. of rows given input.
+-- SUM():
+    Summarize the total of the input columns and gives the result
+-- AVG():
+    It will give the average of the given column like sales
+-- MAX():
+    It find the highest sales in our data. searching for highest value between 
+    multiple values.
+
+-- MIN():
+        It will find the lowest value in our data seaching for lowest value
 */
+
+
+-- find the total number of orders 
+
+USE MyDatabase;
+
+SELECT 
+    COUNT(*) AS total_no_Orders
+FROM orders
+
+
+-- find the total of sales in our data 
+
+SELECT 
+    SUM(Sales) AS Total_sales
+FROM orders
+
+-- find the highest sales of all orders 
+SELECT 
+    MAX(Sales) AS Highest_Sales
+FROM orders
+
+-- find the lowest sales of all orders 
+SELECT 
+    MIN(Sales) AS Lowest_Sales
+FROM Orders 

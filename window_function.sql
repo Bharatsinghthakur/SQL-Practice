ffdaf05efb44337afba7f4c83de05a99efccd878
Bharat Singh Thakur <bharatsinghthakur04@gmail.com>
2026-07-30 20:35:15 +0530

    window_function

---
+++ window_function.sql
@@ -0,0 +1,35 @@
+/*
+WINDOW FUNCTION RULES 
+
+Rule1 : window functions can be used only in SELECT and ORDER BY clause .
+window functions cannot be used to filter data -- using WHERE & GROUP BY clause
+
+Rule2 : Nesting window functions is not allowed!
+
+Rule3 : sql execute window functions after WHERE clause 
+
+Rule4: window functions can be used together with GROUP BY in the same query,
+ONLY if the same columns are used.
+
+*/
+
+-- find the total sales for each order status only for two products 101 and 102
+USE SalesDB;
+
+SELECT 
+    OrderID,
+    OrderDate,
+    OrderStatus,
+    ProductID,
+    Sales,
+    SUM(Sales) OVER (PARTITION BY OrderStatus) TotalSales
+FROM Sales.Orders
+WHERE ProductID IN (101,102)
+
+-- Rank customers based on their total sales
+SELECT 
+    CustomerID,
+    SUM(Sales) TotalSales,
+    RANK() OVER(ORDER BY SUM(Sales) DESC) RankCustomers
+FROM Sales.Orders
+GROUP BY CustomerID

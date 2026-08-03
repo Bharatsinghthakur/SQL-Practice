9744b4eeaeb7ea433688607e10d89f34614d378d
Bharat Singh Thakur <bharatsinghthakur04@gmail.com>
2026-08-03 20:41:38 +0530

    Window_function_usecase

--- window_function.sql
+++ window_function.sql
@@ -227,3 +227,15 @@ SELECT
     ROW_NUMBER() OVER(ORDER BY OrderID,OrderDate) UniqueID,
     *
 FROM Sales.OrdersArchive
+
+-- Indentify Dublicates
+
+-- Indentify dublicates rows in the table 'OrderArchieve'
+-- and return a clean result without any dublicates
+
+SELECT * FROM (
+SELECT 
+ROW_NUMBER() OVER(PARTITION BY OrderID ORDER BY CreationTime DESC) rn,
+* 
+FROM Sales.OrdersArchive
+)t WHERE rn=1

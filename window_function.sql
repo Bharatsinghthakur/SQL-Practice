e5a1eae843b10c09110ad04c608526696b943bf9
Bharat Singh Thakur <bharatsinghthakur04@gmail.com>
2026-08-03 20:17:39 +0530

    Window_function_useCase

--- window_function.sql
+++ window_function.sql
@@ -193,3 +193,27 @@ SELECT
     AVG(Sales) OVER(PARTITION BY ProductID ORDER BY OrderDate) MovingAVG,
     AVG(Sales) OVER(PARTITION BY ProductID ORDER BY OrderDate ROWS BETWEEN CURRENT ROW AND 1 FOLLOWING) RollingAVG
 FROM Sales.Orders
+
+
+-- Top N Analysis 
+-- Find the top highest sales for each product 
+SELECT * 
+FROM (
+SELECT 
+    OrderID,
+    ProductID,
+    Sales,
+    ROW_NUMBER() OVER(PARTITION BY ProductID ORDER BY Sales DESC ) RankByProduct
+ FROM Sales.Orders
+ )t WHERE RankByProduct = 1
+
+ -- find the lowest 2 customers based on their total sales
+SELECT * 
+FROM(
+SELECT
+    CustomerID,
+    SUM(Sales) TotalSales,
+    ROW_NUMBER() OVER(ORDER BY SUM(Sales)) RankCustomers
+FROM Sales.Orders
+GROUP BY 
+CustomerID)t WHERE RankCustomers <= 2

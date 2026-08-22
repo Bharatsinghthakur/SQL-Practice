d34d80b3a36cf9fd4d8b70929181ce5a4c56d0a2
Bharat Singh Thakur <bharatsinghthakur04@gmail.com>
2026-08-22 19:33:14 +0530

    "Database-Architecture"

--- SubQueries.sql
+++ SubQueries.sql
@@ -19,3 +19,24 @@ Solutions:
 - CTAS(create table AS SELECT)
 
 */
+
+/*
+DATABASE ARCHITECTURE 
+- The server is where the database lives.
+
+- DATABASE ENGINE : It is brain of the database,executing multiple operations
+such as storing , rerieving, and managing data within the database.
+
+- STORAGE 
+
+- DISK STORAGE :
+long term memory where data is stored permanently. So its like your disk in pc it 
+stores the data permanently 
+- IT can hold large amount of data
+- speed slow to read & write
+
+- cache storage: fast short-term memeory, where data is stored temporiarily
+speed - extreme fast to read & write 
+capacity - can hold small amount of data
+
+*/

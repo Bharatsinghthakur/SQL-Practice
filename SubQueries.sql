83853c1f4588ab06bcf1205c2b9dffdccd91c1be
Bharat Singh Thakur <bharatsinghthakur04@gmail.com>
2026-08-22 19:26:27 +0530

    "Subqueries"

---
+++ SubQueries.sql
@@ -0,0 +1,21 @@
+/* ADVANCED SQL TECHNIQUES
+
+DATA WAREHOUSE : A special database that collects and integrates data from 
+different sources to enable analytics and support decesion-making
+
+challenges :
+- redundancy
+- performace issues
+- complexity
+- hard to maintain
+- DB stress
+- security
+
+Solutions:
+- Subquery
+- CTE(common table experession)
+- Views
+- Temp Tables
+- CTAS(create table AS SELECT)
+
+*/

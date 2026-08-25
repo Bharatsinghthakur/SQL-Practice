efec6c400ce08f2a4d3135a4d65ab77cd0f3308f
Bharat Singh Thakur <bharatsinghthakur04@gmail.com>
2026-08-25 20:47:57 +0530

    "subquery"

--- SubQueries.sql
+++ SubQueries.sql
@@ -135,4 +135,17 @@ FROM
 	FROM Sales.Orders
 	GROUP BY CustomerID)t
 
+	SELECT 
+*,
+RANK() OVER(ORDER BY TotalSales DESC) CustomerRank
+FROM
+-- subquery
+	(SELECT 
+		CustomerID,
+		SUM(Sales) TotalSales
+	FROM Sales.Orders
+	GROUP BY CustomerID)t
+
+
+
 

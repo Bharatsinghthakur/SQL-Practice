a1959cbefd764786b4ba4ab180cbc30e8ef5c3b4
Bharat Singh Thakur <bharatsinghthakur04@gmail.com>
2026-09-06 22:25:17 +0530

    "subqueries"

--- SubQueries.sql
+++ SubQueries.sql
@@ -332,3 +332,26 @@ check the existence of rows from another table (EXISTS)
 
 */
 
+/* 
+-- Subqueries
+Query inside another query
+Breaks complex query into smaller managebale pieces
+
+USECASES
+create temporary result set
+prepare data before joining Tables
+dynamic & complex filtering 
+check the existence of rows from another table (EXISTS)
+
+*/
+
+
+-- Show the deatils of orders made by customers in Germany 
+
+SELECT 
+* 
+FROM Sales.Orders o
+WHERE EXISTS (SELECT 1 
+			 FROM Sales.Customers c 
+			 WHERE Country = 'Germany'
+			 AND o.CustomerID = c.CustomerID)

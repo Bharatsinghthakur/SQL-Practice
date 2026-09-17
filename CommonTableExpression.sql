1f176d012623dc6737ae3556a42d32eba0315cf3
Bharat Singh Thakur <bharatsinghthakur04@gmail.com>
2026-09-17 20:40:02 +0530

    "CTE"

--- CommonTableExpression.sql
+++ CommonTableExpression.sql
@@ -4,3 +4,18 @@ Temporary, Named result set(virtual table) that can be used multiple times withi
 to simplify and organise complex query.
 
 */
+
+/*
+CTE 
+Improves readablity,modularity, resuablity
+
+once we start executing the query the DB engine start to execute the query and 
+Store the table details into cache
+
+CTE TYPES 
+_ NON RECURSIVE CTE
+a - Standalone CTE 
+b - Nested CTE
+- RECURSIVE CTE
+
+*/
